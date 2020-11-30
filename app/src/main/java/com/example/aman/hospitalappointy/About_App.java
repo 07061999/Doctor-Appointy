cd4b566fde57e01b71cacecc1fba68aeddc9eec5
07061999 <guptamanishkumar987@gmail.com>
2020-11-30 19:35:30 +0530

    first commit

---
+++ app/src/main/java/com/example/aman/hospitalappointy/About_App.java
@@ -0,0 +1,81 @@
+package com.example.aman.hospitalappointy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.res.Configuration;
+import android.support.v7.app.AppCompatDelegate;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+import mehdi.sakout.aboutpage.AboutPage;
+import mehdi.sakout.aboutpage.Element;
+
+public class About_App extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_about_app);
+
+        simulateDayNight(/* DAY */ 1);
+        Element adsElement = new Element();
+        adsElement.setTitle("Advertise with us");
+
+        View aboutPage = new AboutPage(this)
+                .setDescription("This App helps you to make the Doctor's appointment booking process in simple and easy way.You can book an appointment by some click of button anywhere around the world.The App provides Different well-known Doctor by Specialization")
+                .isRTL(false)
+                .setImage(R.mipmap.logo)
+                .addItem(new Element().setTitle("Version 1"))
+                .addItem(adsElement)
+                .addGroup("Connect with us")
+                .addEmail("guptamanishkumar987@gmail.com")
+                .addFacebook("manishkumar.gupta.75470316")
+                .addTwitter("guptamanish0706")
+                .addInstagram("immanish____")
+                .addGitHub("07061999")
+                .addItem(getCopyRightsElement())
+                .create();
+
+        setContentView(aboutPage);
+    }
+
+
+    Element getCopyRightsElement() {
+        Element copyRightsElement = new Element();
+        final String copyrights = String.format(getString(R.string.copy_right), Calendar.getInstance().get(Calendar.YEAR));
+        copyRightsElement.setTitle(copyrights);
+        copyRightsElement.setIconDrawable(R.drawable.about_icon);
+        copyRightsElement.setIconTint(mehdi.sakout.aboutpage.R.color.about_item_icon_color);
+        copyRightsElement.setIconNightTint(android.R.color.white);
+        copyRightsElement.setGravity(Gravity.CENTER);
+        copyRightsElement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(About_App.this, copyrights, Toast.LENGTH_SHORT).show();
+            }
+        });
+        return copyRightsElement;
+    }
+
+    void simulateDayNight(int currentSetting) {
+        final int DAY = 0;
+        final int NIGHT = 1;
+        final int FOLLOW_SYSTEM = 3;
+
+        int currentNightMode = getResources().getConfiguration().uiMode
+                & Configuration.UI_MODE_NIGHT_MASK;
+        if (currentSetting == DAY && currentNightMode != Configuration.UI_MODE_NIGHT_NO) {
+            AppCompatDelegate.setDefaultNightMode(
+                    AppCompatDelegate.MODE_NIGHT_NO);
+        } else if (currentSetting == NIGHT && currentNightMode != Configuration.UI_MODE_NIGHT_YES) {
+            AppCompatDelegate.setDefaultNightMode(
+                    AppCompatDelegate.MODE_NIGHT_YES);
+        } else if (currentSetting == FOLLOW_SYSTEM) {
+            AppCompatDelegate.setDefaultNightMode(
+                    AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+        }
+    }
+}
